dc12435156a4391ac0e866fb80da996d45fedee9
OlehDrivko <116021948+OlehDrivko@users.noreply.github.com>
2024-01-28 17:42:04 +0100

    DodajKlienta(),DodajVIPklienta(),DodajProduct()

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ProjectOOPw68340
         static Queue<Orders> KolejkaZamowien = new Queue<Orders>();
         static List<Product> Produkty = new List<Product>();
         static List<Customer> Klienci = new List<Customer>();
-        static List<Customer> PremiumKlienci = new List<Customer>();
+        static List<PremiumCustomer> PremiumKlienci = new List<PremiumCustomer>();
         static void Main(string[] args)
         {
 
@@ -18,11 +18,15 @@ namespace ProjectOOPw68340
                 Console.WriteLine("Menu:");
                 Console.WriteLine("1. Dodaj Product");
                 Console.WriteLine("2. Zobacz Produkty");
-                Console.WriteLine("2. Dodaj Klienta");
-                Console.WriteLine("3. Dodaj Zamówienie");
-                Console.WriteLine("4. Zobacz zamówienia");
-                Console.WriteLine("5. Przetwórz zamówienie");
-                Console.WriteLine("6. Wejśzie");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("3. Dodaj Klienta");
+                Console.WriteLine("4. Dodaj VIP Klienta");
+                Console.WriteLine("5. Zobacz Klientów");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("6. Dodaj Zamówienie");
+                Console.WriteLine("7. Zobacz zamówienia");
+                Console.WriteLine("8. Przetwórz zamówienie");
+                Console.WriteLine("9. Wejśzie");
 
                 int choice = GetUserChoice();
 
@@ -32,18 +36,30 @@ namespace ProjectOOPw68340
                         DodajProduct();
                         break;
                     case 2:
-                        foreach (var produkt in Produkty)
-                        {
+                            foreach (var produkt in Produkty)
+                            {
                             Console.WriteLine($"Product ID: {produkt.ProductID}, Name: {produkt.Name}, Price: {produkt.PriceOfProduct}");
-                        }
+                            }
                         break;
                     case 3:
+                        DodajKlienta();
                         break;
                     case 4:
+                        DodajVIPklienta();
                         break;
                     case 5:
+                        foreach (var klient in Klienci)
+                        {
+                            Console.WriteLine($"Customer ID: {klient.CustomerID}, Name: {klient.Name}, Age: {klient.Age}");
+                        }
+                        foreach (var PremiumKlienci in PremiumKlienci)
+                        {
+                            Console.WriteLine($"VIP Customer ID: {PremiumKlienci.CustomerID},\n Name: {PremiumKlienci.Name},\n Age: {PremiumKlienci.Age},\n MembershipLevel: {PremiumKlienci.MembershipLevel},\n Discount: {PremiumKlienci.Discount} % ");
+                        }
                         break;
                     case 6:
+                        break;
+                    case 7:
                         Environment.Exit(0);
                         break;
                     default:
@@ -72,6 +88,32 @@ namespace ProjectOOPw68340
                 Produkty.Add(produkt);
                 Console.WriteLine("Produkt dodany pomyślnie.");
 
+            }
+            static void DodajKlienta()
+            {
+                Console.WriteLine("Podaj Imię klienta: ");
+                string CustomerName = Console.ReadLine();
+                Console.WriteLine("Podaj wiek klienta:");
+                double WiekKlienta = double.Parse(Console.ReadLine());
+
+                Customer customer = new Customer { Name = CustomerName, Age = WiekKlienta };
+                Klienci.Add (customer);
+                Console.WriteLine("Klient dodany pomyślnie.");
+            }
+            static void DodajVIPklienta()
+            {
+                Console.WriteLine("Podaj Imię VIP klienta: ");
+                string VIPCustomerName = Console.ReadLine();
+                Console.WriteLine("Podaj wiek VIP klienta:");
+                double WiekVIPKlienta = double.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj związek:");
+                string MembershipLevelWithVIPCUSTOMER = Console.ReadLine();
+                Console.WriteLine("Podaj ile obniżki:");
+                double DiscountVIP = double.Parse(Console.ReadLine());
+
+                PremiumCustomer premiumCustomer = new PremiumCustomer { Name = VIPCustomerName, Age = WiekVIPKlienta , MembershipLevel = MembershipLevelWithVIPCUSTOMER, Discount = DiscountVIP };
+                PremiumKlienci.Add(premiumCustomer);
+                Console.WriteLine(" VIP Klient dodany pomyślnie. ");
             }
 
 
