c157ad7eac377d3fcea9f003dedbf1c89566fa45
OlehDrivko <116021948+OlehDrivko@users.noreply.github.com>
2024-01-28 13:53:36 +0100

    ID, PRoduct, Customer, Orders

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
-﻿namespace ProjectOOPw68340
+﻿using ProjectOOPw68340;
+
+namespace ProjectOOPw68340
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+    }
+
+    class TotalID
+    {
+        public int ProductID = 0 ;
+        public int CustomerID = 0;
+        public int OrderID = 0;
+    }
+
+    class Product :  TotalID
+    {
+        protected int CurentProductID;
+        public string Name { get; set; }
+        public string PriceOfProduct { get; set; }
+
+        public Product(string ProductName, string ProductPriceOfProduct)
+        {
+            this.Name = ProductName;
+            this.PriceOfProduct = ProductPriceOfProduct;
+            CurentProductID++;
+            ProductID = CurentProductID;
+        }
+    }
+
+    class Customer : TotalID
+    {
+        protected int CurentCustomerID;
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public Customer(string CustomerName, double CustomerAge) 
+        {
+            this.Name= CustomerName;
+            this.Age = CustomerAge;
+            CurentCustomerID++;
+            CustomerID = CurentCustomerID;
+        }
+    }
+
+    class Orders : TotalID
+    {
+        protected int CurentOrderID;
+        public Product Product { get; set; }
+        public Customer Customer { get; set; }
+        public Orders(Product product, Customer customer)
+        {
+            CurentOrderID++;
+            OrderID = CurentOrderID;
+            Product = product;
+            Customer = customer;
         }
     }
 }
